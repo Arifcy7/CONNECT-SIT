a744d7b0a7e751c63d7540ea0d42e806fb11a73a
Arifcy7 <choudharyarif756@gmail.com>
2024-09-15 23:16:30 +0530

    teacher portal 30%

--- app/src/main/java/com/example/connectsit/ui/screens/teacher/TeacherPortalScreen.kt
+++ app/src/main/java/com/example/connectsit/ui/screens/teacher/TeacherPortalScreen.kt
@@ -4,12 +4,10 @@ package com.example.connectsit.ui.screens.teacher
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -35,20 +33,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.example.connectsit.R
-import com.example.connectsit.navigation.ScreenC
 import com.example.connectsit.navigation.ScreenK
 
 
@@ -113,7 +104,7 @@ fun yourCourses(){
 @Composable
 fun UploadChoices(category: String,navController: NavController){
     val Bluish = Color(0xFF523EC8)
-    val Buttoncolor = Color(0xFF2A1C9F)
+    val Buttoncolo = Color(0xFF2A1C9F)
         Row(modifier = Modifier
         .size(width = 400.dp, height = 250.dp)
         .padding(horizontal = 20.dp, vertical = 20.dp)
@@ -153,7 +144,7 @@ fun UploadChoices(category: String,navController: NavController){
             Button(
                 onClick = { navController.navigate(ScreenK) }, modifier = Modifier
                     .padding(horizontal = 20.dp, vertical = 20.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Buttoncolor)
+                colors = ButtonDefaults.buttonColors(containerColor = Buttoncolo)
             ) {
                 Text(text = "UPLOAD")
             }
